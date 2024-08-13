2d50f16c315990c0069b8da4c042f0df951477c0
RgrFSPrudenci <159174080+Rgrfsp@users.noreply.github.com>
2024-08-13 14:58:11 -0300

    Add files via upload

---
+++ dump.sqlite (2).sql
@@ -0,0 +1,15 @@
+-- TABLE
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+CREATE TABLE Produtos(
+id int(3) UNIQUE,
+nome VARCHAR(50) NOT NULL,
+preço INT(6) NOT NULL,
+setor VARCHAR(50)
+);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
